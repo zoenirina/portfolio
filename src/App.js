e4accf304ebfd745d571bd0b46c2ed693f9a535b
Marie Viviane <zoenirinamarieviviane@gmail.com>
2024-02-08 11:16:38 +0100

    rectification

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import './App.css'
 import  Header  from './components/Header/Header'
 import  Hero  from './components/Hero/Hero'
 import  Footer  from './components/Footer/Footer'
+import Aos from 'aos';
+import {useEffect} from 'react'
 
 function App() {
+  useEffect(() => {
+    Aos.init()
+  
+  }, [])
   return (
     <div className="App">
       <Header/>
-      <Hero/>
+      <main><Hero/>
+      <h5
+          data-aos="ease-in"
+          data-aos-duration="1800"
+          data-aos-delay="500"
+          className="text-headingColor font-[600] text-[16px] bg-smallTextColor">
+          Bienvenue!
+        </h5>
+      </main>
       <Footer/>
     </div>
   );
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
+import 'aos/dist/aos.css'
+function Hero(){
 
-const Hero = () => {
   return (
-    <section className="pt-0" id='about'>
+    <section className="pt-0 bg-primaryColor" id='about'>
 <div className="container pt-14">
-    <div className="md-flex items-center justify-between sm:flex-col md:flex-row">
-<div className="w-full md:basis-1/2"/>
-    <h5
-    data-aos="fade-right"
-    data-aos-duration="1000"
-    className="text-headingColor font-[600] text-[16px]">
-       I'm a developper 
-    </h5>
+    <div className=" md:flex items-center justify-between sm:flex-col md:flex-row">
+      <div className="w-full md:basis-1/2"/>
+        <h5
+          data-aos="fade-up"
+          data-aos-duration="1800"
+          className="text-headingColor font-[600] text-[16px] bg-smallTextColor">
+          Bienvenue!
+        </h5>
+
+        <h1
+          data-aos="fade-up"
+          data-aos-duration="1500"
+          className="text-headingColor font-[800] text-[1.8rem] ">
+          I'm Zoenirina Marie Viviane <br/>Web Devélopper
+        </h1>
 
     </div>
 </div>
